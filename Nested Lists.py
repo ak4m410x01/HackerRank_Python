f50a9cacb6d22c5c7342c8e0d4d1e357ec5cacf9
ak4m410x01 <abdallah10kamal@gmail.com>
2023-09-26 19:02:56 +0300

    Nested Lists

---
+++ Nested Lists.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    students = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+
+second_highest_score = sorted(set([score for name, score in students]))[1]
+
+print(
+    "\n".join(
+        sorted([name for name, score in students if score == second_highest_score])
+    )
+)
